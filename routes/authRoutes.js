3df64d0cb65843d2c0d0298eae75af0a841a0b5a
avsssai <34810623+avsssai@users.noreply.github.com>
2020-08-12 23:41:57 +0530

    blog optimised for personal use

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -19,65 +19,7 @@ router.get('/register', (req, res) => {
     res.render('auth/register');
 });
 
-router.post('/register', [
-    check('username').isEmail().withMessage("Must be an email address."),
-    check('password').isLength({
-        min: 6
-    }).withMessage("Password must be atleast 6 characters long.")
 
-], (req, res) => {
-
-    // User.register(new User({username: req.body.username}),req.body.password)
-    //     .then(user=>{
-    //         passport.authenticate('local')(req,res,()=>{
-    //             res.redirect('/home');
-    //             console.log(req.user);
-    //         })
-    //     })
-    //     .catch(err=>{
-    //         console.log(err);
-    //         res.redirect('/home/register');
-    //     })
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        // return res.status(422).json({ errors: errors.array() });
-
-        //IF there are validation errors.
-        return res.render('auth/register', {
-            errors: errors.array()
-        });
-
-    }
-
-
-    User.register(new User({
-            username: req.body.username
-        }), req.body.password)
-        .then(user => {
-
-            passport.authenticate('local')(req, res, () => {
-                console.log("new user added.");
-                res.redirect('/home');
-
-            })
-        })
-        .catch(err => {
-
-
-            res.redirect('/register');
-            console.log(err);
-
-
-
-        });
-});
-
-// router.post('/login',passport.authenticate('local',{
-//     successRedirect:'/',
-//     failureRedirect:'/login'
-// }),(req,res)=>{
-
-// });
 router.post('/login',
     passport.authenticate('local', {
         successRedirect: '/',
